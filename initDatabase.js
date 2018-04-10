c3bda3ecd430e16db97f2943f80575fbf1feda36
obellavance <bellavance.o@gmail.com>
2018-04-10 15:09:26 -0400

    ajout des teste,correctif, ajout route.

--- initDatabase.js
+++ initDatabase.js
@@ -21,5 +21,12 @@ MongoClient.connect(url, function(err, client) {
   configCollection.remove({});
   configCollection.insert({"Value" : "Recommandation"});
   configCollection.insert({"Value" : "Configuration"});
+  
+  t configCollection = db.collection('User');
+
+  configCollection.remove({});
+  configCollection.insert({"username" : "client", "password" : "37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5f4133a33f0688f"});
+  configCollection.insert({"username" : "admin", "password" : "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"});
+  
   client.close();
 });
