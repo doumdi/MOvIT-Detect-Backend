f7a561718ea5fe0bf0a44321633b5fb08e32061a
obellavance <bellavance.o@gmail.com>
2018-04-16 16:04:15 -0400

    correction

--- initDatabase.js
+++ initDatabase.js
@@ -21,12 +21,14 @@ MongoClient.connect(url, function(err, client) {
   configCollection.remove({});
   configCollection.insert({"Value" : "Recommandation"});
   configCollection.insert({"Value" : "Configuration"});
-  
-  t configCollection = db.collection('User');
+  configCollection.insert({"Value" : "Goal"});
 
-  configCollection.remove({});
-  configCollection.insert({"username" : "client", "password" : "37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5f4133a33f0688f"});
-  configCollection.insert({"username" : "admin", "password" : "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"});
+  
+  const configUser = db.collection('User');
+
+  configUser.remove({});
+  configUser.insert({"username" : "user", "password" : "37a8eec1ce19687d132fe29051dca629d164e2c4958ba141d5f4133a33f0688f"});
+  configUser.insert({"username" : "clinician", "password" : "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"});
   
   client.close();
 });
