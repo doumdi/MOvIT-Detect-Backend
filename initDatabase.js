df76a2004a49a87e775754cea62abab6728d3097
obellavance <bellavance.o@gmail.com>
2018-10-15 12:46:06 -0400

    remove token validation, add droptable for test.

--- initDatabase.js
+++ initDatabase.js
@@ -22,12 +22,17 @@ MongoClient.connect(url, function(err, client) {
   configCollection.insert({"Value" : "Recommandation"});
   configCollection.insert({"Value" : "Configuration"});
   configCollection.insert({"Value" : "Goal"});
+  
+ const configCollection = db.collection('Current_State');
 
+  configCollection.remove({});
+  configCollection.insert({"Value" : "CurrentState"});
   
   const configUser = db.collection('User');
 
   configUser.remove({});
-  configUser.insert({"username" : "user", "password" : "948FE603F61DC036B5C596DC09FE3CE3F3D30DC90F024C85F3C82DB2CCAB679D"}); //client
+  configUser.insert({"username" : "user", "password" : "948FE603F61DC036B5C596DC09FE3CE3F3D30DC90F024C85F3C82DB2CCAB679D"});
+  //client
   configUser.insert({"username" : "clinician", "password" : "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"}); //admin
   
   client.close();
